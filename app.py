2d806274172bed55ccf8cc5c8c7cdc69c799e9e8
AskarAbdullaev <abdullaev25042000@gmail.com>
2025-08-05 16:35:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ with center_col:
                             length = 5
                             rows = 3
                         elif game.size == 5:
-                            length = 7
+                            length = 8
                             rows = 1
                         elif game.size == 6:
                             length = 6
