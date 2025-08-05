0f3cc7b4d79e363ededc9425ed4de6e3758b813b
AskarAbdullaev <abdullaev25042000@gmail.com>
2025-08-05 16:36:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ with center_col:
                             length = 8
                             rows = 1
                         elif game.size == 6:
-                            length = 6
+                            length = 10
                             rows = 1
                         placeholder = '\n'.join(['\u00A0' * length for _ in range(rows)])
                         if st.button(placeholder, key=f"valid_{i}_{j}"):
